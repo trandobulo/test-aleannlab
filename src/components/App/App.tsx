f1e6dcb66e04db4575990f30ff34ff9f670a0147
trandobulo <trandobulo@gmail.com>
2022-11-23 11:02:05 +0200

    delete react-scripts package

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,7 +16,7 @@ function App(): JSX.Element {
 
   const getJobList = (): void => {
     const url: string =
-      "https://api.json-generator.com/temlates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu";
+      "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu";
 
     fetch(url)
       .then((response: Response) => {
